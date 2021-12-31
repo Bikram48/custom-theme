d4799e6746876510d1f6c49426d5b7f69f2cc7d4
Bikram48 <chandbikram001@gmail.com>
2021-12-31 18:23:56 +0545

    add - styles on pages

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
     get_header();
     if ( have_posts() ):
         while ( have_posts() ) : the_post(); ?>
+        <article class="post">
             <h2><a href="<?php the_permalink() ?>"><?php the_title() ?></a></h2>
 <?php
-            the_content();
+            the_content(); 
+?>
+        </article>
+<?php
         endwhile;
     else :
         echo '<p>There are no posts!</p>';
